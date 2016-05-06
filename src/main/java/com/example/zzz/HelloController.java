2c65b0026b1de6267ba20785b9edf9100421cd7f
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-05-06 16:51:22 +0900

    [add]create war file for deployment to Tomcat

--- src/main/java/com/example/zzz/HelloController.java
+++ src/main/java/com/example/zzz/HelloController.java
@@ -3,13 +3,23 @@ package com.example.zzz;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	
-	@RequestMapping("/")
+
+	@RequestMapping(name = "/", method = RequestMethod.GET)
 	public String sayHello(Model model) {
-		model.addAttribute("message", "Hello world!!");
+		model.addAttribute("message", "Hello World!!");
 		return "index";
 	}
+
+	@RequestMapping(name = "/", method = RequestMethod.POST)
+	public String createHelloMessage(@RequestParam("name") String name, Model model) {
+		model.addAttribute("message", "Hello World!!" + name + "さん");
+		return "index";
+
+	}
+
 }
--- src/main/java/com/example/zzz/MyappApplication.java
+++ src/main/java/com/example/zzz/MyappApplication.java
@@ -2,11 +2,19 @@ package com.example.zzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyappApplication {
+public class MyappApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
 	}
+
 }
